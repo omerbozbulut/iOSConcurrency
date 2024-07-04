b8c5c576d863a99c886bff7e1839deb4ada0d7a6
Ömer Bozbulut <fbozbulut@invio.com>
2024-07-04 15:31:54 +0300

    ISTAKIBI-?? Asenkron İşlemlerin Sunulması
    + project init

---
+++ iOSConcurrency/iOSConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSConcurrencyApp.swift
+//  iOSConcurrency
+//
+//  Created by Ömer Bozbulut on 3.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
